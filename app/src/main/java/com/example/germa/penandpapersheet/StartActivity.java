caa18f93bfcbf7aec6898f5a284a42b96c1e4dfe
Robert Philippsohn <st148766@stud.uni-stuttgart.de>
2018-01-09 16:54:32 +0100

    added the new activity which will be the main activity and added the button functionality

--- app/src/main/java/com/example/germa/penandpapersheet/StartActivity.java
+++ app/src/main/java/com/example/germa/penandpapersheet/StartActivity.java
@@ -1,13 +1,28 @@
 package com.example.germa.penandpapersheet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button btContinue;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btContinue = (Button) findViewById(R.id.btContinue);
+
+        btContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                Intent mainIntent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
